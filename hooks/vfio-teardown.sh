0004c793bf9720a77ad855f976173617ddcc514d
SuperHexBot <65476506+SuperHexBot@users.noreply.github.com>
2022-10-07 03:00:37 +0800

    Update vfio-teardown.sh

--- hooks/vfio-teardown.sh
+++ hooks/vfio-teardown.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 10
 DATE=$(date +"%m/%d/%Y %R:%S :")
 
 
@@ -10,34 +9,28 @@ cat /sys/bus/pci/devices/0000:28:00.0/reset_method
 echo 'device_specific' > /sys/bus/pci/devices/0000:28:00.0/reset_method
 cat /sys/bus/pci/devices/0000:28:00.0/reset_method
 
-echo "$DATE reattach 1"
-virsh nodedev-reattach pci_0000_28_00_1
 echo "$DATE reattach 0"
 virsh nodedev-reattach pci_0000_28_00_0
+echo "$DATE reattach 1"
+virsh nodedev-reattach pci_0000_28_00_1
 
-sleep 10
+
+
 echo "$DATE load drvie1r amdgpu"
 modprobe  amdgpu
-echo "$DATE load drvie1r amdgpu ok"
 
 
-#echo "$DATE load drvier gpu_sched"
 #modprobe  gpu_sched
-#echo "$DATE load drvier ttm"
 #modprobe  ttm
-#echo "$DATE load drvier drm_kms_helper"
 #modprobe  drm_kms_helper
-#echo "$DATE load drvier i2c_algo_bit"
 #modprobe  i2c_algo_bit
-#echo "$DATE load drvier drm"
 #modprobe  drm
-#echo "$DATE load drvier snd_hda_intel"
 #modprobe  snd_hda_intel
 
 
 
 echo "$DATE sleep"
-sleep 10
+sleep 5
 # Restart Display Manager
 echo "$DATE start display"
 systemctl start display-manager.service
